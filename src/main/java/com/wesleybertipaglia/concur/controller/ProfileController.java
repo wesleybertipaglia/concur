653bb4189076ff30d6aefd473e59565e8e552c5d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-10-01 11:35:11 -0300

    feat: update Profile

--- src/main/java/com/wesleybertipaglia/concur/controller/ProfileController.java
+++ src/main/java/com/wesleybertipaglia/concur/controller/ProfileController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wesleybertipaglia.concur.record.UserDetailsRecord;
+import com.wesleybertipaglia.concur.record.user.UserDetailsRecord;
 import com.wesleybertipaglia.concur.service.ProfileService;
 
 @RestController
--- src/main/java/com/wesleybertipaglia/concur/service/ProfileService.java
+++ src/main/java/com/wesleybertipaglia/concur/service/ProfileService.java
@@ -1,7 +1,5 @@
 package com.wesleybertipaglia.concur.service;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -9,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.wesleybertipaglia.concur.model.User;
-import com.wesleybertipaglia.concur.record.UserDetailsRecord;
+import com.wesleybertipaglia.concur.record.user.UserDetailsRecord;
 import com.wesleybertipaglia.concur.repository.UserRepository;
 
 @Service
@@ -23,13 +21,15 @@ public class ProfileService {
         Jwt jwt = (Jwt) authentication.getPrincipal();
         String userId = jwt.getSubject();
 
-        User user = userRepository.findById(UUID.fromString(userId))
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         return new UserDetailsRecord(
                 user.getId(),
                 user.getName(),
                 user.getUsername(),
-                user.getEmail());
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getUpdatedAt());
     }
 }
